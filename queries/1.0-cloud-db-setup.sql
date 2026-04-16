eb2d8c031763252a3690d1e78be39e2f345ded8b
tejakusumahyusuf832 <tejakusumahyusuf832@gmail.com>
2026-04-16 21:21:18 +0700

    Add a query file to set up a cloud database in Neon

--- queries/1.0-cloud-db-setup.sql
+++ queries/1.0-cloud-db-setup.sql
@@ -26,13 +26,14 @@ CREATE TABLE skz_stats (
     PRIMARY KEY (video_id, scraped_at)
 );
 
--- 4. Comments (Snapshots of top comments)
+-- 4. Comments (Snapshots of top comments, tracked over time)
 CREATE TABLE skz_comments (
-    comment_id VARCHAR(100) PRIMARY KEY,
+    comment_id VARCHAR(100),
     video_id VARCHAR(50) REFERENCES skz_snippets(video_id),
     author VARCHAR(100),
     text TEXT,
     like_count INT,
     published_at TIMESTAMP WITH TIME ZONE,
-    scraped_at TIMESTAMP WITH TIME ZONE
+    scraped_at TIMESTAMP WITH TIME ZONE,
+    PRIMARY KEY (comment_id, scraped_at)
 );
