1693f121ecb46870172d5e55667b1dfcec3eff43
tejakusumahyusuf832 <tejakusumahyusuf832@gmail.com>
2026-06-04 16:27:09 +0700

    Finalize src/analyses/sentiment.py to perform sentiment analysis

--- src/analyses/sentiment.py
+++ src/analyses/sentiment.py
@@ -1,14 +1,17 @@
 import os
+from pathlib import Path
 
 import fasttext
 from huggingface_hub import hf_hub_download, login
 from loguru import logger
-import matplotlib.pyplot as plt
 import polars as pl
-import seaborn as sns
+import torch
 from transformers import AutoModelForSequenceClassification, AutoTokenizer, pipeline
+import typer
 
-from src.config import MODELS_DIR
+from src.config import INTERIM_DATA_DIR, MODELS_DIR
+
+app = typer.Typer()
 
 query = """
     SELECT DISTINCT ON (comment_id) 
@@ -24,123 +27,143 @@ query = """
         scraped_at DESC
 """
 
-# === FUNCTION ===
-HF_TOKEN = os.environ.get("HF_TOKEN", "")
-if not HF_TOKEN:
-    logger.error("HF_TOKEN not found.")
 
-login(token=HF_TOKEN)
+def detect_language(text_series: pl.Series) -> pl.Series:
+    HF_TOKEN = os.environ.get("HF_TOKEN", "")
+    if not HF_TOKEN:
+        logger.error("HF_TOKEN not found.")
 
-model_path = hf_hub_download(
-    repo_id="facebook/fasttext-language-identification",
-    filename="model.bin",
-    local_dir=MODELS_DIR / "fasttext",
-    local_files_only=True,
-)
-lang_model = fasttext.load_model(model_path)
+    login(token=HF_TOKEN)
 
+    model_path = hf_hub_download(
+        repo_id="facebook/fasttext-language-identification",
+        filename="model.bin",
+        local_dir=MODELS_DIR / "fasttext",
+        local_files_only=True,
+    )
 
-def batch_detect_language(text_series: pl.Series) -> pl.Series:
+    lang_model = fasttext.load_model(model_path)
     text_list = text_series.to_list()
     labels, _ = lang_model.predict(text_list, k=1)
     cleaned_labels = [lbl[0].replace("__label__", "") for lbl in labels]
     return pl.Series(cleaned_labels)
 
 
-# ----------------
+def get_sentiment_labels(text_series: pl.Series) -> pl.Series:
+    model_id = "cardiffnlp/twitter-xlm-roberta-base-sentiment"
+    local_dir = MODELS_DIR / "hugging-face"
 
-# === FUNCTION ===
-model_id = "cardiffnlp/twitter-xlm-roberta-base-sentiment"
-local_dir = MODELS_DIR / "hugging-face"
+    try:
+        tokenizer = AutoTokenizer.from_pretrained(local_dir, local_files_only=True)
+        model = AutoModelForSequenceClassification.from_pretrained(
+            local_dir, local_files_only=True
+        )
+    except Exception:
+        logger.warning("Model missing or incomplete locally. Downloading from Hugging Face...")
 
-try:
-    tokenizer = AutoTokenizer.from_pretrained(local_dir, local_files_only=True)
-    model = AutoModelForSequenceClassification.from_pretrained(local_dir, local_files_only=True)
-except Exception:
-    logger.warning("Model missing or incomplete locally. Downloading from Hugging Face...")
+        tokenizer = AutoTokenizer.from_pretrained(model_id)
+        model = AutoModelForSequenceClassification.from_pretrained(model_id)
+        tokenizer.save_pretrained(local_dir)
+        model.save_pretrained(local_dir)
+        logger.success("Download complete and safely stored!")
 
-    tokenizer = AutoTokenizer.from_pretrained(model_id)
-    model = AutoModelForSequenceClassification.from_pretrained(model_id)
-    tokenizer.save_pretrained(local_dir)
-    model.save_pretrained(local_dir)
-    logger.success("Download complete and safely stored!")
+    sentiment_pipeline = pipeline(
+        "sentiment-analysis",  # type: ignore
+        model=model,
+        tokenizer=tokenizer,
+        truncation=True,
+        max_length=512,
+        device=0,
+        dtype=torch.float16,
+    )
 
-
-sentiment_pipeline = pipeline(
-    "sentiment-analysis",  # type: ignore
-    model=model,
-    tokenizer=tokenizer,
-    truncation=True,
-    max_length=512,
-)
-
-
-def perform_sentiment_analysis(text_series: pl.Series) -> pl.Series:
     text_list = text_series.to_list()
-    results = sentiment_pipeline(text_list)
+    results = sentiment_pipeline(text_list, batch_size=64)
     sentiment_labels = [result["label"] for result in results]
     return pl.Series(sentiment_labels)
 
 
-# ----------------
+@app.command()
+def perform_sentiment_analysis(
+    db_uri_key: str = typer.Option(
+        "DB_URI_KEY", help="URI key of the database containing your data."
+    ),
+    output_path: Path = INTERIM_DATA_DIR / "df_sentiment_final_result.parquet",
+    return_data: bool = False,
+):
+    DB_URI = os.environ.get(db_uri_key, "")
+    if not DB_URI:
+        logger.error("Database URI not found.")
 
-db_uri_key = "DB_URI_KEY"
+    logger.info("Fetching comment data from database...")
+    try:
+        df_lazy = pl.read_database_uri(query, DB_URI).lazy()
+        logger.success("Comment data fetched successfully.")
+    except Exception as e:
+        logger.error(f"Failed to fetch comment data from database: {e}")
 
-DB_URI = os.environ.get(db_uri_key, "")
-if not DB_URI:
-    logger.error("Database URI not found.")
+    logger.info("Starting to detect languages...")
 
-logger.info("Fetching comment data from database...")
-try:
-    df_lazy = pl.read_database_uri(query, DB_URI).lazy()
-    logger.success("Comment data fetched successfully.")
-except Exception as e:
-    logger.error(f"Failed to fetch comment data from database: {e}")
+    # Label every comment with its language
+    df_lang_detected = df_lazy.with_columns(
+        pl.col("text")
+        .fill_null("")
+        .str.replace_all("\n", " ", literal=True)
+        .map_batches(detect_language, return_dtype=pl.String)
+        .alias("language")
+    )
 
-logger.info("Starting to detect languages...")
+    # Select only the languages that mainly appear 90% of the data
+    df_selected_lang = (
+        df_lang_detected.select(pl.col("language").value_counts(sort=True))
+        .unnest("language")
+        .with_columns((pl.col("count") * 100 / pl.col("count").sum()).alias("lang_percentage"))
+        .with_columns(pl.col("lang_percentage").cum_sum().alias("cum_sum"))
+        .filter(pl.col("cum_sum") <= 90)
+    )
+
+    selected_lang_list = df_selected_lang.collect().get_column("language").to_list()
+
+    hf_langs = {
+        "arb_Arab",
+        "eng_Latn",
+        "fra_Latn",
+        "deu_Latn",
+        "hin_Deva",
+        "ita_Latn",
+        "spa_Latn",
+        "por_Latn",
+    }
+
+    # Combine the selected languages with the languages supported by Hugging Face
+    chosen_langs = hf_langs | set(selected_lang_list)
+    chosen_langs_list = list(chosen_langs)
+
+    df_selected_labels = df_lang_detected.filter(pl.col("language").is_in(chosen_langs_list))
+
+    df_sentiment_result = (
+        df_selected_labels.with_columns(
+            pl.col("text")
+            .map_batches(get_sentiment_labels, return_dtype=pl.String)
+            .alias("sentiment_label")
+        )
+        .select(
+            pl.col("video_id", "comment_id"),
+            pl.when(pl.col("sentiment_label") == "positive")
+            .then(pl.lit(1))
+            .otherwise(pl.lit(0))
+            .alias("is_positive"),
+        )
+        .group_by(pl.col("video_id"))
+        .agg(pl.col("is_positive").mean().alias("positive_sentiment_percentage"))
+        .with_columns(pl.col("positive_sentiment_percentage").mul(100))
+    )
+
+    if return_data:
+        return df_sentiment_result
+    else:
+        df_sentiment_result.sink_parquet(output_path)
 
 
-df_lang_labeled = df_lazy.with_columns(
-    pl.col("text")
-    .fill_null("")
-    .str.replace_all("\n", " ", literal=True)
-    .map_batches(batch_detect_language, return_dtype=pl.String)
-    .alias("language")
-)
-
-
-df_selected_lang = (
-    df_lang_labeled.select(pl.col("language").value_counts(sort=True))
-    .unnest("language")
-    .with_columns((pl.col("count") * 100 / pl.col("count").sum()).alias("lang_percentage"))
-    .with_columns(pl.col("lang_percentage").cum_sum().alias("cum_sum"))
-    .filter(pl.col("cum_sum") <= 90)
-)
-
-selected_lang_list = df_selected_lang.collect().get_column("language").to_list()
-
-hf_langs = {
-    "arb_Arab",
-    "eng_Latn",
-    "fra_Latn",
-    "deu_Latn",
-    "hin_Deva",
-    "ita_Latn",
-    "spa_Latn",
-    "por_Latn",
-}
-
-chosen_langs = hf_langs | set(selected_lang_list)
-chosen_langs_list = list(chosen_langs)
-
-df_selected_labels = df_lang_labeled.filter(pl.col("language").is_in(chosen_langs_list))
-
-df_selected_labels.head(8).collect()
-
-df_sentiment_labels = df_selected_labels.with_columns(
-    pl.col("text")
-    .map_batches(perform_sentiment_analysis, return_dtype=pl.String)
-    .alias("sentiment_label")
-)
-
-df_sentiment_labels_eager = df_sentiment_labels.collect()
+if __name__ == "__main__":
+    app()
